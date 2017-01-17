cf69bd2ef19b60e9614d371a19132d8417c39080
Liam Collins <liam.collins@superawesome.tv>
2017-01-17 10:08:35 +0000

    rendering playlists only if they exist

--- client/components/competitions/Comp.js
+++ client/components/competitions/Comp.js
@@ -11,14 +11,20 @@ const Comp = React.createClass({
     const i = this.props.competitions.findIndex((comp) => comp.id === parseInt(compId));
     const comp = this.props.competitions[i];
 
+    let playlists;
+    if (comp.playlists && comp.playlists.length) {
+      playlists = <div className="playlists-grid">
+        {comp.playlists.map((playlist,i) =>
+          <Playlist {...this.props} key={i} i={i} playlist={playlist}/>
+        )}
+      </div>
+    } else {
+      playlists = <div>No playlists</div>
+    }
     return (
       <div className="comp">
         <h2 className="grid-title">{comp.name}</h2>
-        <div className="playlists-grid">
-          {comp.playlists.map((playlist,i) =>
-            <Playlist {...this.props} key={i} i={i} playlist={playlist}/>
-          )}
-        </div>
+        {playlists}
       </div>
     )
   }
